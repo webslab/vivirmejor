4a08a7b554d5d08d871282732858e46c420e4b58
kennycallado <kennycallado@hotmail.com>
2024-12-03 19:49:37 +0100

    UPDATE
    about:
    - using images instead of url
    - move about images into its own directory
    - implementing into about page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,20 @@
 import { defineCollection, z } from "astro:content";
 
+type Image = {
+	image: () => z.ZodTypeAny;
+};
+
 const membersCollection = defineCollection({
 	type: "data",
-	schema: z.object({
-		id: z.number(),
-		group: z.number(),
-		name: z.string(),
-		position: z.string(),
-		department: z.string(),
-		photo: z.string().url(),
-	}),
+	schema: ({ image }: Image) =>
+		z.object({
+			id: z.number(),
+			group: z.number(),
+			name: z.string(),
+			position: z.string(),
+			department: z.string(),
+			photo: image(),
+		}),
 });
 
 export const collections = {
