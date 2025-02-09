55a076348f46c06732bfeb19fecea5662c531688
kennycallado <kennycallado@hotmail.com>
2025-02-09 21:54:52 +0100

    FIX(lib)
    consts: wrong WEBSLAB_SITE

--- src/_includes/lib/consts.ts
+++ src/_includes/lib/consts.ts
@@ -2,8 +2,8 @@ export const WEBSLAB_PROJECT = "base";
 export const WEBSLAB_DOMAIN = "ipsitec.es";
 export const WEBSLAB_TOKEN = "TOKEN"; // TODO: temporary, should be user token
 
-export const WEBSLAB_SITE = `http://localhost:8000`;
-// export const WEBSLAB_SITE = `https://${WEBSLAB_PROJECT}.${WEBSLAB_DOMAIN}`;
+// export const WEBSLAB_SITE = `http://localhost:8000`;
+export const WEBSLAB_SITE = `https://${WEBSLAB_PROJECT}.${WEBSLAB_DOMAIN}`;
 
 export const SITE_TITLE = "Base";
 export const SITE_DESCRIPTION = "Welcome to Base!";
