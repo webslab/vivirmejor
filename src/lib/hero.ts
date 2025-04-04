5c25e0f420a80d1576230940333355c2d6744496
kennycallado <kennycallado@hotmail.com>
2025-04-04 10:16:59 +0200

    FIX(components)
    HeroPicker: actually allow pick an image as hero image

--- src/lib/hero.ts
+++ src/lib/hero.ts
@@ -1,12 +1,11 @@
 import { Jodit } from "jodit/es2021/jodit.min.js";
 import { editor } from "$lib/services/editor.ts";
+import { WEBSLAB_SITE } from "$lib/consts.ts";
 
-// type File = { type: string; name: string };
+type Images = { baseUrl: string; files: string[]; type: "image"[] };
 
 const heroPreview = document.getElementById("hero-preview") as HTMLImageElement;
 const heroPicker = document.getElementById("hero-picker") as HTMLButtonElement;
-const heroModal = document.getElementById("hero-modal")!;
-const modalSave = heroModal!.querySelector(".btn-primary")!;
 const hero = document.getElementById("hero") as HTMLInputElement;
 
 const fb = new Jodit.modules.FileBrowser({
@@ -18,73 +17,17 @@ const fb = new Jodit.modules.FileBrowser({
 	...editor.getOptions().filebrowser,
 });
 
-modalSave.addEventListener("click", () => {
-	const modalClose = heroModal.querySelector(".btn-secondary") as HTMLButtonElement;
-	const select = heroModal.querySelector("select")!;
-
-	if (select.value) {
-		const event = new CustomEvent("hero:modal:data", { detail: select.value });
-		heroModal.dispatchEvent(event);
-
-		modalClose.click();
-	}
-});
-
-heroModal.addEventListener("hero:modal:data", (event) => {
-	const { detail } = event as CustomEvent;
-
-	hero.value = detail;
-	heroPreview.src = detail;
-});
-
 heroPicker.addEventListener("click", () => {
-	fb.open((images: any) => {
-		console.log(images);
-	});
+	const origin = WEBSLAB_SITE;
+
+	fb.open((images: Images) => {
+		if (images.files.length === 1) {
+			hero.value = images.baseUrl || origin + images.files[0];
+			heroPreview.src = images.baseUrl || origin + images.files[0];
+
+			return;
+		}
+
+		alert("Please select an image from the list.");
+	}, true);
 });
-
-/**
-heroPicker.addEventListener("click", async () => {
-	const response = await fetch("/filefind/index.php?action=files", {
-		method: "GET",
-		headers: {
-			"Accept": "application/json",
-			"X-WEBSLAB-Project": WEBSLAB_PROJECT,
-			"X-WEBSLAB-Token": WEBSLAB_TOKEN, // authService.getToken(),
-		},
-	});
-
-	response
-		.json()
-		.then(({ data }) => {
-			const heroBody = heroModal.querySelector(".modal-body")!;
-			const heroSelect = heroBody.querySelector("select")!;
-			const baseurl: string = data.sources[0].baseurl;
-			const files: File[] = data.sources[0].files;
-
-			const values: string[] = [];
-
-			files.forEach((file) => {
-				if (file.type === "image") {
-					values.push(
-						`https://${WEBSLAB_PROJECT}.${WEBSLAB_DOMAIN}${baseurl}${file.name}`,
-					);
-				}
-			});
-
-			// clear the select
-			heroSelect.innerHTML = "";
-
-			values.forEach((value, index) => {
-				const option = document.createElement("option");
-
-				option.value = value;
-				option.innerText = files[index].name;
-
-				heroSelect.appendChild(option);
-			});
-
-			heroBody.innerHTML = heroSelect.outerHTML;
-		});
-});
-*/
