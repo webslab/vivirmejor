c77426f032f2b1f040b282984a15042b1b9318ca
kennycallado <kennycallado@hotmail.com>
2025-05-06 17:45:51 +0200

    update(package)
    - initial changes
    - base to vivirmejor

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,12 +1,12 @@
-export const WEBSLAB_PROJECT = "base";
+export const WEBSLAB_PROJECT = "vivirmejor";
 export const WEBSLAB_DOMAIN = "ipsitec.es";
 export const WEBSLAB_TOKEN = "TOKEN"; // TODO: temporary, should be user token
 
 // export const WEBSLAB_SITE = `http://localhost:8000`;
 export const WEBSLAB_SITE = `https://${WEBSLAB_PROJECT}.${WEBSLAB_DOMAIN}`;
 
-export const SITE_TITLE = "Base";
-export const SITE_DESCRIPTION = "Welcome to Base!";
+export const SITE_TITLE = "Vivirmejor";
+export const SITE_DESCRIPTION = "Welcome to Vivirmejor!";
 
 // TODO: Change to production URL
 export const DB = {
